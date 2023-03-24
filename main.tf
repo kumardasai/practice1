f22d8b29ce728f6f9e65bf12573e6073772d781e
kumardasai <75432589+kumardasai@users.noreply.github.com>
2023-03-24 19:54:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+     terraform {
+       backend "remote" {
+         # The name of your Terraform Cloud organization.
+         organization = "kumardasaipersonal"
+
+         # The name of the Terraform Cloud workspace to store Terraform state files in.
+        workspaces {
+           name = "kumardasaipersonal-workspace"
+         }
+       }
+     }
+
 provider "aws" {
     region = "us-east-1"
 }
