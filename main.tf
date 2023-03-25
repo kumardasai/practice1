86ddc652678bc3c116b9dfdfaf2b174003a10435
kumardasai <75432589+kumardasai@users.noreply.github.com>
2023-03-25 19:44:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.60.0"
+    }
+  }
+}
+
 provider "aws" {
   profile = "testing"
   region = "us-east-1"
