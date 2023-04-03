185dbb17676367328f020bf26b8eeb83e2428eea
kumardasai <75432589+kumardasai@users.noreply.github.com>
2023-04-03 23:00:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.74.0"
-      region  = "us-east-1"
-    }
-  }
+provider "aws" {
+    shared_credentials_files = ["~/.aws/credentials"]
 }
 
 resource "aws_vpc" "ntier" {
